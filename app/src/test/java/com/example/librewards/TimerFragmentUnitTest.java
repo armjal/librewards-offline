fd8cbc6e0709901ebd9eafd0bfbf94fb77fc7dad
Arman J <arman_jn@hotmail.co.uk>
2025-07-27 18:58:26 +0100

    improvement: separate functionality of setPointsCalculator. Add parameterized test for it"

--- app/src/test/java/com/example/librewards/TimerFragmentUnitTest.java
+++ app/src/test/java/com/example/librewards/TimerFragmentUnitTest.java
@@ -1,17 +1,36 @@
 package com.example.librewards;
 
-import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import static org.junit.Assert.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import java.util.stream.Stream;
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
-public class ExampleUnitTest {
-    @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+
+class TimerFragmentUnitTest {
+    static Stream<Arguments> provideInputsForPointsCalculatorTest() {
+        return Stream.of(
+                Arguments.of(5, 0),
+                Arguments.of(10500, 400),
+                Arguments.of(120000, 125),
+                Arguments.of(185000, 225),
+                Arguments.of(800000, 700)
+
+        );
+    }
+    @ParameterizedTest
+    @MethodSource("provideInputsForPointsCalculatorTest")
+    void calculatePointsFromTime_givenRangeOfTimes_returnsExpectedPointsEarned(long timeSpentInLibrary, int expectedPointsEarned) {
+        TimerFragment timerFragment = new TimerFragment();
+
+        int actualPointsEarned = timerFragment.calculatePointsFromTimeSpent(timeSpentInLibrary);
+        assertEquals(expectedPointsEarned, actualPointsEarned);
+
     }
 }

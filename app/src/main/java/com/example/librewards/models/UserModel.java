7c6db6af5aecaed7af4471e1cca4493735d44a2c
Arman J <arman_jn@hotmail.co.uk>
2025-07-28 23:33:45 +0100

    chore: remove redundant listener logic from usermodel

--- app/src/main/java/com/example/librewards/models/UserModel.java
+++ app/src/main/java/com/example/librewards/models/UserModel.java
@@ -3,14 +3,9 @@ package com.example.librewards.models;
 public class UserModel {
     private String name = "";
     private Integer points = 0;
-    private UserChangeListener userChangeListener;
 
-    public void setUserChangeListener(UserChangeListener userChangeListener){
-        this.userChangeListener = userChangeListener;
-    }
     public void setPoints(Integer points) {
         this.points = points;
-        userChangeListener.onPointsChanged(points);
     }
 
     public void setName(String name) {

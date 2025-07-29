92372e328e3c0654bbcfa7881fe36d5bb790b9e9
Arman J <arman_jn@hotmail.co.uk>
2025-07-29 23:50:01 +0100

    refactor: initial refactor of RewardsFragment including removal of unnecessary comments, unused functions, re-order of conditional logic for clarity

--- app/src/main/java/com/example/librewards/RewardsFragment.java
+++ app/src/main/java/com/example/librewards/RewardsFragment.java
@@ -1,14 +1,6 @@
-/*Author: Arman Jalilian
-Date of Completion: 07/06/2020
-Module Code: CSC3122
-Application Name: Lib Rewards
-Application Purpose: Rewards students as they spend time at the library
-Class Name: RewardsFragment
-Class Purpose: The rewards fragment is where a user can spend the points that they have earned. They can acquire a code from the library shop which will decrease the amount of
-points, if they have enough. This will prove to the library staff that they have spent the points and therefore they can in return reward them.
- */
-
 package com.example.librewards;
+
+import static java.util.Objects.requireNonNull;
 
 import android.app.Dialog;
 import android.graphics.Color;
@@ -30,20 +22,15 @@ import androidx.fragment.app.Fragment;
 import com.example.librewards.models.UserChangeListener;
 import com.example.librewards.models.UserChangeNotifier;
 
-import java.util.ArrayList;
 import java.util.List;
 
 
 public class RewardsFragment extends Fragment implements UserChangeListener {
-    private static final String TAG = RewardsFragment.class.getSimpleName();
-    Dialog popup;
-    DatabaseHelper myDb;
-    private ListFromFile listFromFile;
-    private EditText editText;
+    private static final String REWARDS_TAG = RewardsFragment.class.getSimpleName();
+
+    private DatabaseHelper myDb;
     private TextView points;
     private TextView name;
-    private Button rewardButton;
-    public List<String> rewardsCodes = new ArrayList<>();
     private String textToEdit;
 
     @Override
@@ -52,44 +39,32 @@ public class RewardsFragment extends Fragment implements UserChangeListener {
         // Inflate the layout for this fragment
         View v = inflater.inflate(R.layout.fragment_rewards, container, false);
         //Assigns the field to the view's specified in the fragment_timer XML file file
-        myDb = new DatabaseHelper(getActivity().getApplicationContext());
-        rewardButton = v.findViewById(R.id.rewardButton);
-        editText = v.findViewById(R.id.rewardText);
+        myDb = new DatabaseHelper(requireActivity());
+        Button rewardButton = v.findViewById(R.id.rewardButton);
+        EditText editText = v.findViewById(R.id.rewardText);
         points = v.findViewById(R.id.points2);
         points.setText(String.valueOf(myDb.getPoints()));
         name = v.findViewById(R.id.nameRewards);
         //Adds the codes from the text file to the database and updates the database every time in case there are new codes or costs in the text file
-        rewardsCodes = addNewCodes("rewardcodes.txt");
+        List<String> rewardsCodes = addNewRewardCodes();
         myDb.updateRewardCodes(rewardsCodes);
 
         //Sets actions on clicking the "Reward" Button
-        rewardButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Checks if the input text is empty
-                if(editText.length() == 0){
-                    toastMessage("No code was entered, please try again");
+        rewardButton.setOnClickListener(v1 -> {
+            if(editText.length() == 0){
+                toastMessage("No code was entered, please try again");
+            }
+            else if(!rewardsCodes.contains(editText.getText().toString())) {
+                toastMessage(getString(R.string.invalidCode));
+            } else {
+                if (myDb.getPoints() <= myDb.getCost(editText.getText().toString())) {
+                    showPopup(getString(R.string.insufficientFunds));
                 }
-                //Checks if the list containing the reward codes has the one inputted by the user
-                else if(rewardsCodes.contains(editText.getText().toString())){
-                    //Checks if the cost that comes with the code is greater than the points a user has
-                    if(!(myDb.getPoints() <= myDb.getCost(editText.getText().toString()))){
-                        //If the user has enough points, the cost is deducted from the points and the user can get their reward
-                        myDb.minusPoints(myDb.getCost(editText.getText().toString()));
-                        showPopup("Code accepted, keep it up! Your new points balance is: " + myDb.getPoints());
-                        points.setText(String.valueOf(myDb.getPoints()));
-                        //Communicates the new point balance with other fragment
-                        UserChangeNotifier.notifyPointsChanged(myDb.getPoints());
-                    }
-                    //If the points are less than the cost of the reward, a popup shows up stating that the user has insufficient funds
-                    else{
-                        showPopup(getString(R.string.insufficientFunds));
-
-                    }
-                }
-                //If code is incorrect, a Toast will pop up stating invalid code
-                else{
-                    toastMessage(getString(R.string.invalidCode));
+                else {
+                    myDb.minusPoints(myDb.getCost(editText.getText().toString()));
+                    showPopup("Code accepted, keep it up! Your new points balance is: " + myDb.getPoints());
+                    points.setText(String.valueOf(myDb.getPoints()));
+                    UserChangeNotifier.notifyPointsChanged(myDb.getPoints());
                 }
             }
         });
@@ -105,39 +80,30 @@ public class RewardsFragment extends Fragment implements UserChangeListener {
     }
 
     public void showPopup(String text){
-        popup = new Dialog(getActivity());
-        popup.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        Dialog popup = new Dialog(requireActivity());
+        requireNonNull(popup.getWindow()).setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
         popup.setContentView(R.layout.popup_layout);
         ImageView closeBtn = popup.findViewById(R.id.closeBtn);
         TextView popupText = popup.findViewById(R.id.popupText);
         setTextToEdit(text);
         popupText.setText(getTextToEdit());
-        closeBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                popup.dismiss();
-            }
-        });
+        closeBtn.setOnClickListener(v -> popup.dismiss());
         popup.show();
 
     }
-    //Method to set the name on first start-up. Method is called in MainActivity
-    public void initialSetName(){
-        name.setText(getString(R.string.Hey)+ " "+  myDb.getName());
-    }
-    //Method that adds new reward codes to a list using the ListFromFile class
-    private List<String> addNewCodes(String path){
+
+    private List<String> addNewRewardCodes(){
+        ListFromFile listFromFile;
         List<String> newList;
-        listFromFile = new ListFromFile(getActivity().getApplicationContext());
-        newList = listFromFile.readRewardsLine(path);
+        listFromFile = new ListFromFile(requireActivity());
+        newList = listFromFile.readRewardsLine(getString(R.string.rewardcodes_file_name));
         for (String s : newList)
-            Log.d(TAG, s);
+            Log.d(REWARDS_TAG, s);
         return newList;
     }
 
-    //Method creating a custom Toast message
     public void toastMessage(String message){
-        Toast.makeText(getActivity().getApplicationContext(),message,Toast.LENGTH_LONG).show();
+        Toast.makeText(requireActivity() ,message,Toast.LENGTH_LONG).show();
     }
 
     public void setTextToEdit(String textToEdit) {
@@ -146,11 +112,6 @@ public class RewardsFragment extends Fragment implements UserChangeListener {
 
     public String getTextToEdit() {
         return textToEdit;
-    }
-
-    //Method that is used between fragments to update each other's points
-    public void updatedPoints(int newPoints){
-        points.setText(String.valueOf(newPoints));
     }
 
     @Override

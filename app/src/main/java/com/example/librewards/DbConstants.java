c9ff03766c7be8e0db68e07e7d6a75d52f187501
armanjalilian <87311642+armanjalilian@users.noreply.github.com>
2025-08-23 15:05:16 +0100

    refactor: remove ListFromFile class in favour of codes stored among application code (#11)
    * refactor: rename all DB variables
    
    * refactor: reformat file
    
    * refactor: start to remove the need for the ListForFile class by moving code assets into the variables within the application code considering updating them will involve someone to publish a new version of the app either way
    
    * refactor: addInitialCodes to use the codes set within resources of application code
    
    * refactor: remove redundant ListFromFile class along with all usages in favour of codes/cost lists
    
    * refactor: delete redundant codes text files
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

--- app/src/main/java/com/example/librewards/DbConstants.java
+++ app/src/main/java/com/example/librewards/DbConstants.java
@@ -1,9 +1,10 @@
 package com.example.librewards;
 
 public class DbConstants {
+    public static final String DATABASE_NAME = "librewards.db";
     public static final String START_CODES_TABLE_NAME = "start_codes_table";
     public static final String STOP_CODES_TABLE_NAME = "stop_codes_table";
-
-    public static final String START_CODES_FILE_NAME = "startcodes.txt";
-    public static final String STOP_CODES_FILE_NAME = "stopcodes.txt";
+    public static final String REWARD_CODES_TABLE_NAME = "reward_codes_table";
+    public static final String POINTS_TABLE_NAME = "points_table";
+    public static final String NAME_TABLE_NAME = "name_table";
 }
---
+++ app/src/main/java/com/example/librewards/resources/TimerCodes.java
@@ -0,0 +1,21 @@
+package com.example.librewards.resources;
+
+import java.util.List;
+
+public class TimerCodes {
+    public static final List<String> startCodes = List.of(
+            "583927",
+            "493284",
+            "284295",
+            "103948",
+            "405938",
+            "949284");
+
+    public static final List<String> stopCodes = List.of(
+            "241859",
+            "234982",
+            "439052",
+            "863712",
+            "520500",
+            "599385");
+}

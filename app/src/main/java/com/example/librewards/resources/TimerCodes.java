10e0f7c8fe945020db649ad8c9d8fc03a6d87958
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-08-23 13:49:18 +0100

    refactor: addInitialCodes to use the codes set within resources of application code

--- app/src/main/java/com/example/librewards/resources/TimerCodes.java
+++ app/src/main/java/com/example/librewards/resources/TimerCodes.java
@@ -3,17 +3,19 @@ package com.example.librewards.resources;
 import java.util.List;
 
 public class TimerCodes {
-    public static List<Integer> startCodes = List.of(583927,
-            493284,
-            284295,
-            103948,
-            405938,
-            949284);
+    public static List<String> startCodes = List.of(
+            "583927",
+            "493284",
+            "284295",
+            "103948",
+            "405938",
+            "949284");
 
-    public static List<Integer> stopCodes = List.of(241859,
-            234982,
-            439052,
-            863712,
-            520500,
-            599385);
+    public static List<String> stopCodes = List.of(
+            "241859",
+            "234982",
+            "439052",
+            "863712",
+            "520500",
+            "599385");
 }

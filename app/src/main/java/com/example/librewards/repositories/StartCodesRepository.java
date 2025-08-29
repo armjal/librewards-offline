8fbaff645db2daa673f530f1efe7298c1f0d0d9d
armanjalilian <87311642+armanjalilian@users.noreply.github.com>
2025-08-29 12:03:13 +0100

    improvement: retire Codes models in favour of retrieval of code(s) from DB (#19)
    * improvement: retire CodesManager classes in favour of CRUD operation on a code directly from the DB. Timer/CodesRepository classes for both Start/Stop and ensure resources related to DB such as table name are contained only here
    
    * refactor: rename TimerRepository to CodesRepository
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

---
+++ app/src/main/java/com/example/librewards/repositories/StartCodesRepository.java
@@ -0,0 +1,24 @@
+package com.example.librewards.repositories;
+
+import static com.example.librewards.DbConstants.START_CODES_TABLE_NAME;
+import static com.example.librewards.resources.TimerCodes.startCodes;
+
+import com.example.librewards.DatabaseHelper;
+
+import java.util.List;
+
+public class StartCodesRepository extends CodesRepository {
+    public StartCodesRepository(DatabaseHelper dbHelper) {
+        super(dbHelper);
+    }
+
+    @Override
+    public String getTableName() {
+        return START_CODES_TABLE_NAME;
+    }
+
+    @Override
+    public List<String> getOriginalCodes() {
+        return startCodes;
+    }
+}

2cb4e48a40516fe695e37d33a3bd4227ce714525
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-08-29 11:58:54 +0100

    improvement: retire CodesManager classes in favour of CRUD operation on a code directly from the DB. Timer/CodesRepository classes for both Start/Stop and ensure resources related to DB such as table name are contained only here

--- app/src/main/java/com/example/librewards/views/TimerHandler.java
+++ app/src/main/java/com/example/librewards/views/TimerHandler.java
@@ -1,22 +1,15 @@
 package com.example.librewards.views;
+
 import android.os.SystemClock;
 
-import com.example.librewards.controllers.codes.StartCodesManager;
-import com.example.librewards.controllers.codes.StopCodesManager;
-
 public class TimerHandler {
-    private final StartCodesManager startCodesManager;
-    private final StopCodesManager stopCodesManager;
     private final TimerView timerView;
 
-    public TimerHandler(TimerView timerView, StartCodesManager startCodesManager, StopCodesManager stopCodesManager) {
-        this.startCodesManager = startCodesManager;
-        this.stopCodesManager = stopCodesManager;
+    public TimerHandler(TimerView timerView) {
         this.timerView = timerView;
     }
 
-    public void start(String inputtedCode) {
-        startCodesManager.removeUsedCode(inputtedCode);
+    public void start() {
         timerView.changeTimerToDesiredState("start");
         timerView.getTimer().setOnChronometerTickListener(chronometer -> {
             if ((SystemClock.elapsedRealtime() - timerView.getTimer().getBase()) >= 500000) {
@@ -25,8 +18,7 @@ public class TimerHandler {
         });
     }
 
-    public long stop(String inputtedCode) {
-        stopCodesManager.removeUsedCode(inputtedCode);
+    public long stop() {
         timerView.changeTimerToDesiredState("stop");
 
         return SystemClock.elapsedRealtime() - timerView.getTimer().getBase();

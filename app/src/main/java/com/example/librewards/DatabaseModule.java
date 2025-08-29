f52954de92d576a39c38d113f8242be26a3d2f8e
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-08-29 21:21:55 +0100

    feat: perform dependency injection using Hilt

---
+++ app/src/main/java/com/example/librewards/DatabaseModule.java
@@ -0,0 +1,22 @@
+package com.example.librewards;
+
+import android.content.Context;
+
+import javax.inject.Singleton;
+
+import dagger.Module;
+import dagger.Provides;
+import dagger.hilt.InstallIn;
+import dagger.hilt.android.qualifiers.ApplicationContext;
+import dagger.hilt.components.SingletonComponent;
+
+@Module
+@InstallIn(SingletonComponent.class)
+public class DatabaseModule {
+
+    @Provides
+    @Singleton
+    public static DatabaseHelper provideDbHelper(@ApplicationContext Context context) {
+        return new DatabaseHelper(context);
+    }
+}
---
+++ app/src/main/java/com/example/librewards/LibRewardsApp.java
@@ -0,0 +1,9 @@
+package com.example.librewards;
+
+import android.app.Application;
+
+import dagger.hilt.android.HiltAndroidApp;
+
+@HiltAndroidApp
+public class LibRewardsApp extends Application {
+}

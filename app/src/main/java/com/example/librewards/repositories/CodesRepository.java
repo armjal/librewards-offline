bfbc566177098a166831203ddf7a0e488131e98f
armanjalilian <87311642+armanjalilian@users.noreply.github.com>
2025-08-29 12:09:41 +0100

    refactor: clean up CodesRepository, functions and constants (#20)
    * refactor: update private functions, drop the term "Timer", move functions around
    
    * refactor: extract used/unused code vals to constant
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

--- app/src/main/java/com/example/librewards/repositories/CodesRepository.java
+++ app/src/main/java/com/example/librewards/repositories/CodesRepository.java
@@ -12,6 +12,9 @@ import com.example.librewards.DatabaseHelper;
 import java.util.List;
 
 public abstract class CodesRepository {
+    public static final String USED_CODE_VAL = "true";
+    public static final String UNUSED_CODE_VAL = "false";
+
     private final SQLiteDatabase db;
     private final DatabaseHelper dbHelper;
 
@@ -20,13 +23,23 @@ public abstract class CodesRepository {
         db = dbHelper.getWritableDatabase();
     }
 
-    public void deleteTimerCode(String code) {
+    public abstract String getTableName();
+
+    public abstract List<String> getOriginalCodes();
+
+    public String get(String value) {
+        return dbHelper.getString(getTableName(), CODES_COLUMN_NAME,
+                USED_CODE_COLUMN_NAME + " = ? AND " + CODES_COLUMN_NAME + " = ?",
+                new String[]{UNUSED_CODE_VAL, value});
+    }
+
+    public void delete(String code) {
         ContentValues contentValues = new ContentValues();
-        contentValues.put(USED_CODE_COLUMN_NAME, "true");
+        contentValues.put(USED_CODE_COLUMN_NAME, USED_CODE_VAL);
         db.update(getTableName(), contentValues, CODES_COLUMN_NAME + " = ?", new String[]{code});
     }
 
-    public void storeTimerCodes() {
+    public void populate() {
         for (String code : getOriginalCodes()) {
             ContentValues contentValues = new ContentValues();
             contentValues.put(CODES_COLUMN_NAME, code);
@@ -34,32 +47,23 @@ public abstract class CodesRepository {
         }
     }
 
-    public void checkForTimerCodeUpdates() {
-        List<String> allCodes = getAllCodes(getTableName());
-        if (!(allCodes.equals(getOriginalCodes()))) {
-            this.updateTimerCodes(getTableName(), getOriginalCodes());
+    public void checkForUpdates() {
+        List<String> originalCodes = getOriginalCodes();
+        List<String> allCodes = getAll();
+        if (!(allCodes.equals(originalCodes))) {
+            this.update(getTableName(), originalCodes);
         }
     }
 
-    public void updateTimerCodes(String table, List<String> newCodesList) {
+    private List<String> getAll() {
+        return dbHelper.getAllStrings(getTableName(), CODES_COLUMN_NAME, null, null);
+    }
+
+    private void update(String table, List<String> newCodesList) {
         ContentValues contentValues = new ContentValues();
         for (int i = 1, k = 0; i < newCodesList.size() - 1; i++, k++) {
             contentValues.put(CODES_COLUMN_NAME, newCodesList.get(k));
             db.update(table, contentValues, ID_COLUMN_NAME + " = ?", new String[]{String.valueOf(i)});
         }
-    }
-
-    public List<String> getAllCodes(String table) {
-        return dbHelper.getAllStrings(table, CODES_COLUMN_NAME, null, null);
-    }
-
-    public abstract String getTableName();
-
-    public abstract List<String> getOriginalCodes();
-
-    public String getCode(String value) {
-        return dbHelper.getString(getTableName(), CODES_COLUMN_NAME,
-                USED_CODE_COLUMN_NAME + " = ? AND " + CODES_COLUMN_NAME + " = ?",
-                new String[]{"false", value});
     }
 }

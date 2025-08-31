5387637f82845a2f77fc40145e4a58189ce8d27b
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-08-31 11:46:17 +0100

    test: add case for starting timer successfully

--- app/src/androidTest/java/com/example/librewards/TimerFragmentInstrumentedTest.java
+++ app/src/androidTest/java/com/example/librewards/TimerFragmentInstrumentedTest.java
@@ -1,6 +1,8 @@
 package com.example.librewards;
 
 import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
@@ -15,15 +17,14 @@ import android.os.Bundle;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 import androidx.test.platform.app.InstrumentationRegistry;
 
-import com.example.librewards.models.UserRepositoryFake;
+import com.example.librewards.repositories.StartCodesRepositoryFake;
+import com.example.librewards.repositories.UserRepositoryFake;
 import com.example.librewards.views.TimerFragment;
 
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-
-import java.util.Map;
 
 import javax.inject.Inject;
 
@@ -38,18 +39,23 @@ public class TimerFragmentInstrumentedTest {
     public HiltAndroidRule hiltRule = new HiltAndroidRule(this);
     @Inject
     UserRepositoryFake userRepositoryFake;
+    @Inject
+    StartCodesRepositoryFake startCodesRepositoryFake;
     private static final int POINTS_VALUE_ID = R.id.points;
     private static final int NAME_VALUE_ID = R.id.nameTimer;
     private static final int POINTS_LABEL_ID = R.id.textView2;
     private static final int START_BUTTON_ID = R.id.startButton;
+    private static final int START_TEXT_ID = R.id.startText;
     private static final int STOP_BUTTON_ID = R.id.stopButton;
     private static final int TIMER_ID = R.id.timer;
 
     @Before
     public void setUp() {
         hiltRule.inject();
-        Bundle bundle = new Bundle();
+        startCodesRepositoryFake.populate();
         userRepositoryFake.setUser("test-name", 0);
+
+        Bundle bundle = new Bundle();
         bundle.putParcelable("user", userRepositoryFake.getUser());
 
         launchFragmentInHiltContainer(TimerFragment.class, bundle, R.style.AppTheme, null);
@@ -69,5 +75,15 @@ public class TimerFragmentInstrumentedTest {
         onView(withId(START_BUTTON_ID)).check(matches(isDisplayed())).check(matches(withText("start")));
         onView(withId(STOP_BUTTON_ID)).check(matches(not(isDisplayed()))).check(matches(withText("stop")));
         onView(withId(TIMER_ID)).check(matches(isDisplayed())).check(matches(withText("00:00")));
+    }
+
+    @Test
+    public void test_timerFragment_startsTimerWithCorrectCode() throws InterruptedException {
+        onView(withId(START_TEXT_ID)).perform(typeText("123456"));
+        onView(withId(START_BUTTON_ID)).perform(click());
+        onView(withId(STOP_BUTTON_ID)).check(matches(isDisplayed()));
+        Thread.sleep(1000);
+        onView(withId(TIMER_ID)).check(matches(not(withText("00:00"))));
+        onView(withId(START_BUTTON_ID)).check(matches(not(isDisplayed())));
     }
 }

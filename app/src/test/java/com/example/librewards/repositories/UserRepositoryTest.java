0856e53d75a7e0bdd83780884a200b48e5d4eb87
Arman Jalilian <arman.jalilian@krakenflex.com>
2025-09-06 21:51:35 +0100

    test: add more repo unit tests

--- app/src/test/java/com/example/librewards/repositories/UserRepositoryTest.java
+++ app/src/test/java/com/example/librewards/repositories/UserRepositoryTest.java
@@ -2,6 +2,7 @@ package com.example.librewards.repositories;
 
 import static org.hamcrest.CoreMatchers.equalTo;
 import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.Assert.assertThrows;
 
 import android.os.Build;
 
@@ -36,17 +37,51 @@ public class UserRepositoryTest {
     public void setUp() {
         hiltAndroidRule.inject();
         userRepo = new UserRepository(databaseHelper);
-        insertUserInDb();
     }
 
     @Test
     public void test_userRepo_getUser_successfullyReturnsAUserObjectWithValuesFromDb() {
+        insertUserInDb();
+
         UserModel user = userRepo.getUser();
 
         assertThat(user, equalTo(new UserModel(1, "John", 100)));
     }
 
-    public void insertUserInDb() {
+    @Test
+    public void test_userRepo_getUser_givenNoUsersInDb_throwsNullPointerException() {
+        assertThrows(NullPointerException.class, () -> userRepo.getUser());
+    }
+
+    @Test
+    public void test_userRepo_addName_successfullyAddsUserNameToDb() {
+        userRepo.addName("Test Name");
+
+        userRepo.getUser();
+        UserModel expectedUser = new UserModel(1, "Test Name", 0);
+
+        assertThat(userRepo.getUser(), equalTo(expectedUser));
+    }
+
+    @Test
+    public void test_userRepo_addPoints_successfullyAddsPointsToDb() {
+        insertUserInDb();
+
+        userRepo.addPoints(userRepo.getUser(), 10);
+
+        assertThat(userRepo.getPoints(), equalTo(110));
+    }
+
+    @Test
+    public void test_userRepo_minusPoints_successfullyMinusesPointsToDb() {
+        insertUserInDb();
+
+        userRepo.minusPoints(userRepo.getUser(), 10);
+
+        assertThat(userRepo.getPoints(), equalTo(90));
+    }
+
+    private void insertUserInDb() {
         databaseHelper.getWritableDatabase().execSQL("INSERT INTO user_table (name, points) VALUES ('John', 100)");
     }
 }

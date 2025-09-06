1980b1146e04df427641d31231434ddbf3cd10bf
armanjalilian <87311642+armanjalilian@users.noreply.github.com>
2025-09-06 18:44:13 +0100

    test(including app fix): introduce RewardsRepo test suite including fixes that were presented from them (#29)
    * chore: setup reward codes retrieval for tests by returning from function instead of static field
    
    * test: introduce RewardsRepository test suite including test returning code in Db
    
    * test: add further RewardsRepo tests including a helper method to get all info from Rewards table
    
    * fix: ensure that rewards are replaced correctly given new rewards list and also still populates on first execution
    
    ---------
    
    Co-authored-by: Arman Jalilian <arman.jalilian@krakenflex.com>

--- app/src/testFixtures/java/com/example/librewards/resources/RewardCodesTest.java
+++ app/src/testFixtures/java/com/example/librewards/resources/RewardCodesTest.java
@@ -5,5 +5,7 @@ import java.util.Map;
 public class RewardCodesTest {
     public static final Map<String, Integer> rewardCodesAndPoints = Map.of(
             "123456", 5,
-            "654321", 10);
+            "654321", 10,
+            "876544", 50,
+            "342134", 2);
 }
